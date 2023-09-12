3e9121eac3d2a49b72439aae6492eb41233ec904
RodolfoTorija <rodolfo.torija07@gmail.com>
2023-09-12 16:01:35 -0600

    JS para redirigir a mi cuenta

---
+++ script.js
@@ -0,0 +1,8 @@
+// Selecciona el botón por su ID
+var botonIniciarSesion = document.getElementById('iniciarSesion');
+
+// Agrega un evento de clic al botón
+botonIniciarSesion.addEventListener('click', function() {
+    // Redirige al enlace de Facebook
+    window.location.href = 'https://www.facebook.com/AnthonyTorija/';
+});
